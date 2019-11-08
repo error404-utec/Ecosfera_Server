8c5eda2dbaea1c4ac39d05c972665e7ddf119566
joserovere <57200274+joserovere@users.noreply.github.com>
2019-11-08 20:54:55 -0300

    controles departamentos

--- ECOSFERA_MARK1/ejbModule/com/services/DepartamentoBean.java
+++ ECOSFERA_MARK1/ejbModule/com/services/DepartamentoBean.java
@@ -10,6 +10,7 @@ import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 
 import com.entities.Departamento;
+import com.entities.Localidad;
 import com.exceptions.ServiciosException;
 
 /**
@@ -20,12 +21,10 @@ import com.exceptions.ServiciosException;
 public class DepartamentoBean implements DepartamentoBeanRemote {
 	@PersistenceContext
 	private EntityManager em;
-    /**
-     * Default constructor. 
-     */
+
     public DepartamentoBean() {
-        // TODO Auto-generated constructor stub
     }
+    
     @Override
    	public void crear(Departamento departamento) throws ServiciosException {
    		try{
@@ -75,5 +74,58 @@ public class DepartamentoBean implements DepartamentoBeanRemote {
 	public Departamento obtenerporID(Long id) {
 		TypedQuery<Departamento> query = em.createQuery("select d from Departamento d WHERE d.id LIKE :id",Departamento.class).setParameter("id", id);
 		return query.getSingleResult();
+	}
+	@Override
+	public String controles_postCreate(Departamento departamento) {
+		String error = "";
+		TypedQuery<Departamento> query = em.createQuery("select d from Departamento d WHERE d.nombre LIKE :nombre",Departamento.class).setParameter("nombre", departamento.getNombre());
+   		List<Departamento> lista = query.getResultList();
+		for(Departamento elemento : lista) {
+			if(elemento.getNombre().equalsIgnoreCase(departamento.getNombre())) {
+				error= "El nombre ingresado ya existe en el sistema.";
+				break;
+			}
+		}
+		
+		if(error.isEmpty()) {
+			System.out.println();
+			query = em.createQuery("select d from Departamento d WHERE d.codigo LIKE :codigo",Departamento.class).setParameter("codigo", departamento.getCodigo());
+	   		lista.removeAll(lista);
+			lista = query.getResultList();
+			for(Departamento elemento : lista) {
+				error= "El código ingresado ya existe en el sistema.";
+				break;
+			}
+		}
+		
+		return error;
+	}
+
+
+	
+	@Override
+	public String controles_preDelete(Departamento departamento) {
+		String error = "";
+		TypedQuery<Departamento> query = em.createQuery("select d from Departamento d WHERE d.id LIKE :id",Departamento.class).setParameter("id", departamento.getId());
+		Departamento departamento2 = query.getSingleResult();
+   		List<Localidad> colloc = departamento2.getLocalidades();
+		if (!colloc.isEmpty()) {
+			error= "No se puede eliminar el departamento, cuenta con localidades asociadas.";
+		}
+		return error;
+	}
+
+	@Override
+	public String controles_postModify(Departamento departamento) {
+		String error = "";
+		TypedQuery<Departamento> query = em.createQuery("select d from Departamento d WHERE d.nombre LIKE :nombre",Departamento.class).setParameter("nombre", departamento.getNombre());
+   		List<Departamento> lista = query.getResultList();
+		for(Departamento elemento : lista) {
+			if(elemento.getNombre().equalsIgnoreCase(departamento.getNombre()) && elemento.getId()!= departamento.getId()) {
+				error= "El nombre ingresado ya existe en el sistema.";
+				break;
+			}
+		}
+		return error;
 	}
 }
